cd18401fd698a988e8e7141461829680fc656a41
Xavi Gil <xgil78@gmail.com>
2016-05-12 23:24:37 +0200

    Open the folder of the last used simulator in Finder

--- sim/main.swift
+++ sim/main.swift
@@ -8,5 +8,55 @@
 
 import Foundation
 
-print("Hello, World!")
+import Foundation
 
+let homePath = NSHomeDirectory()
+let simFolder = "\(homePath)/Library/Developer/CoreSimulator/Devices/"
+let simFolderUrl = NSURL(fileURLWithPath: simFolder)
+
+let fileManager = NSFileManager.defaultManager()
+
+guard fileManager.fileExistsAtPath(simFolder) else {
+    print("error path not found \(simFolder)")
+    exit(1)
+}
+
+let enumerator = fileManager.enumeratorAtURL(
+    simFolderUrl,
+    includingPropertiesForKeys: [NSURLNameKey, NSURLIsDirectoryKey, NSURLAttributeModificationDateKey],
+    options: [.SkipsHiddenFiles, .SkipsSubdirectoryDescendants ],
+    errorHandler: nil)!
+
+var lastModifiedFolder = (name: "", date: NSDate(timeIntervalSince1970: 0))
+while let folder = enumerator.nextObject() as? NSURL{
+    var filenameValue: AnyObject?
+    var modifiedDateValue: AnyObject?
+    var isDirectoryValue: AnyObject?
+    try folder.getResourceValue(&isDirectoryValue, forKey: NSURLIsDirectoryKey)
+    guard let isDirectory = isDirectoryValue as? Bool else {
+        break
+    }
+    if isDirectory {
+        try folder.getResourceValue(&filenameValue, forKey: NSURLNameKey)
+        try folder.getResourceValue(&modifiedDateValue, forKey: NSURLAttributeModificationDateKey)
+        let folderInfo = (name: filenameValue as! String, date: modifiedDateValue as! NSDate)
+        if folderInfo.date.compare(lastModifiedFolder.date) == .OrderedDescending {
+            lastModifiedFolder = folderInfo
+        }
+    }
+}
+print(lastModifiedFolder)
+
+let lastSimFolder = "\(simFolder)\(lastModifiedFolder.name)/data/Containers/Data/Application"
+print(lastSimFolder)
+
+func shell(args: String...) -> Int32 {
+    let task = NSTask()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
+exit(shell("open", lastSimFolder))
